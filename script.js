58ae4f6d0111198f13bb756b36c6ad2560988a1e
Mukul Singh <dakshmukul15@gmail.com>
2024-05-10 13:44:23 +0530

    icon animation & progress bar animation

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const checkBoxList = document.querySelectorAll(".custom-checkbox");
 const inputFields = document.querySelectorAll(".goal-input");
 const progressBar = document.querySelector(".progress-bar");
+var completedCount = 0;
+
+window.addEventListener("load", function () {
+  inputFields.forEach((input) => {
+    const savedValue = localStorage.getItem(input.id);
+    if (savedValue) {
+      input.value = savedValue;
+    }
+  });
+});
 
 checkBoxList.forEach((checkBox) => {
   checkBox.addEventListener("click", (e) => {
@@ -10,6 +20,8 @@ checkBoxList.forEach((checkBox) => {
 
     if (allFieldFilled) {
       checkBox.parentElement.classList.toggle("completed");
+      // console.log(checkBox.parentElement.classList[1]);
+      updateProgress(checkBox.parentElement.classList[1]);
     } else {
       progressBar.classList.add("show-error");
     }
@@ -17,8 +29,27 @@ checkBoxList.forEach((checkBox) => {
 });
 
 inputFields.forEach((input) => {
+  input.addEventListener("input", function () {
+    localStorage.setItem(input.id, input.value);
+  });
+
   input.addEventListener("focus", function () {
     progressBar.classList.remove("show-error");
   });
 });
-localStorage.setItem("inputValues", inputFields);
+
+function updateProgress(className) {
+  if (className === "completed") {
+    completedCount++;
+  } else if (className === undefined) {
+    completedCount--;
+  }
+
+  const progressValue = document.querySelector(".progress-value");
+
+  const widthValue = completedCount * 33.33;
+  progressValue.style.width = `${widthValue}%`;
+  progressValue.children[0].textContent = `${completedCount}/3 Completed`;
+}
+
+window.addEventListener("load", updateProgress);
